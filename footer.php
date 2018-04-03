b72a1eeb1bbe6c99a7df33019e057d7bf7679995
Axel Loupias <axel.loupias@theprology.com>
2018-04-03 20:26:08 +0200

    Hero CSS Finished

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 
-
-
-
+<footer class="border-top text-center pt-4">
+  <p class="lead">Calle Marqués de la Valdavia 76, 3ºB</p>
+  <p class="lead">Telefono: 605477410</p>
+</footer>
 <?php wp_footer() ?>
 </body>
 </html>
